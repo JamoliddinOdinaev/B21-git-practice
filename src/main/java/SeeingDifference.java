64bbb8361f73e9fb22ba8af2308deef96d468a20
JamoliddinOdinaev <jemo555@gmail.com>
2021-01-26 16:38:19 -0500

    new changes

--- src/main/java/SeeingDifference.java
+++ src/main/java/SeeingDifference.java
@@ -3,4 +3,6 @@ public class SeeingDifference {
     // this is new line of work in movie4
     // this is new line of work in movie5
     // this line of work has something movie5 does not see
+
+    // I have more work to do
 }
